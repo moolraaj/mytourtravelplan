385db6b1c47a0e2a83ef1930b96bc034e134e70f
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-23 15:00:39 +0530

    updates

--- src/app/api/v1/(whatsapp)/whatsapp/route.js
+++ src/app/api/v1/(whatsapp)/whatsapp/route.js
@@ -1,40 +1,40 @@
-import { NextResponse } from 'next/server';
-import messagebird from 'messagebird';
+// import { NextResponse } from 'next/server';
+// import messagebird from 'messagebird';
 
-const messageBirdClient = messagebird(process.env.MESSAGEBIRD_ACCESS_KEY);
+// const messageBirdClient = messagebird(process.env.MESSAGEBIRD_ACCESS_KEY);
 
-export async function POST(req) {
-  try {
-    const { to, message } = await req.json();
+// export async function POST(req) {
+//   try {
+//     const { to, message } = await req.json();
 
-    const params = {
-      to: to,
-      from: '+916230097248',  
-      type: 'text',
-      content: {
-        text: message
-      }
-    };
+//     const params = {
+//       to: to,
+//       from: '+916230097248',  
+//       type: 'text',
+//       content: {
+//         text: message
+//       }
+//     };
 
-    return new Promise((resolve, reject) => {
-      messageBirdClient.conversations.send(params, (err, response) => {
-        if (err) {
-          console.error('Error sending WhatsApp message:', err);
-          resolve(
-            NextResponse.json({ success: false, error: err.message }, { status: 500 })
-          );
-        } else {
-          console.log('Message sent:', response);
-          resolve(
-            NextResponse.json({ success: true, response }, { status: 200 })
-          );
-        }
-      });
-    });
-  } catch (error) {
-    console.error('Error processing request:', error);
-    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
-  }
-}
+//     return new Promise((resolve, reject) => {
+//       messageBirdClient.conversations.send(params, (err, response) => {
+//         if (err) {
+//           console.error('Error sending WhatsApp message:', err);
+//           resolve(
+//             NextResponse.json({ success: false, error: err.message }, { status: 500 })
+//           );
+//         } else {
+//           console.log('Message sent:', response);
+//           resolve(
+//             NextResponse.json({ success: true, response }, { status: 200 })
+//           );
+//         }
+//       });
+//     });
+//   } catch (error) {
+//     console.error('Error processing request:', error);
+//     return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+//   }
+// }
 
  
