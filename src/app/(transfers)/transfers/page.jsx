48e4344fb45af9f0c8691fd830002145fa1b4ef9
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-23 16:05:00 +0530

    updates

--- src/app/(transfers)/transfers/page.jsx
+++ src/app/(transfers)/transfers/page.jsx
@@ -1,48 +1,26 @@
-
-'use client';
-
 import Layout from '@/app/_common/layout/layout'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import CarOptions from '../Components/transferinnersection'
 import WelcomeCard from '../Components/sidebar'
 import Topbanner from '@/app/_common/layout/topbanner'
-import LatestBlog from '@/Components/blogs';
 import transferbg from '../../assets/home_images/transfer-bg.png';
-import { EXPORT_ALL_APIS } from '@/utils/apis/api';
-import TransferPage from './TransferPage';
+import TransferPage from './TransferPage'
 function page() {
-
-  const [blogs, setBlogs] = useState([]);
-
-  const fetchData = async () => {
-    let api = EXPORT_ALL_APIS();
-    let data;
-    data = await api.loadAllBlogs();
-    setBlogs(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [])
-
-
-
-
   return (
     <Layout>
-      <Topbanner />
+        <Topbanner/>
 
-      <div className='vechile-sidebar' style={{ backgroundImage: `url(${transferbg.src})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-        <div className="inner-w-container vechile-sidebar-inner">
-          <CarOptions />
-          <WelcomeCard />
+        <div className='vechile-sidebar'style={{ backgroundImage: `url(${transferbg.src})`, backgroundRepeat: 'no-repeat' , backgroundPosition: 'center' }}>
+          <div className="inner-w-container vechile-sidebar-inner"> 
+            <CarOptions/>
+            <WelcomeCard/>
+          </div> 
         </div>
-      </div>
 
-      <div className='blog_custom'>
-        <TransferPage/>
-      </div>
-
+        <div className='blog_custom'>
+          <TransferPage />
+        </div>
+        
     </Layout>
   )
 }
