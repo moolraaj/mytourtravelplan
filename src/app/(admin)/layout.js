7321d0bd4927c77887c18e240a1bd126a70288c5
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-26 14:44:18 +0530

    updates

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -4,9 +4,10 @@ import NextAuthProvide from "@/provider/authProvider";
 import AdminLayout from "./_common/layout/adminLayout";
 
 export const metadata = {
-  title: "Admin Dashboard",
-  description: "Admin panel for managing the application",
+  title: "Admin Dashboard - My Trav Plan",
+  description: "Manage and oversee travel plans, user accounts, and application settings from a centralized admin panel. Efficiently handle bookings, packages, and user interactions to ensure smooth operations and enhance user experience.",
 };
+
 
 export default function AdminRootLayout({ children }) {
   return (
