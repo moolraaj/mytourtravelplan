59060cf35dbce24ab2a42a24e32d88d7eb23f004
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-08-26 15:52:35 +0530

    updates

--- src/app/(countries)/countries/components/countryPage.jsx
+++ src/app/(countries)/countries/components/countryPage.jsx
@@ -1,52 +1,44 @@
 import LoadingBar from '@/app/_common/innerLoader/innerLoader';
 import Image from 'next/image';
 import Link from 'next/link';
- 
- 
-
 
 const CountryPage = ({ country }) => {
+  if (!country || country.length === 0) {
+    return <div className="explorations"><LoadingBar /></div> ;
+  }
 
   return (
     <div className="explorations">
       <div className="explorations-grid">
-        {country===null||country===undefined?(<LoadingBar/>):(country?.map((exploration, index) => (
+        {country.map((exploration, index) => (
           <div key={index} className="exploration-item">
-            <Link href={`/countries/${exploration.slug}`}  >
-             
+            <Link href={`/countries/${exploration.slug}`}>
               {exploration.images && exploration.images.length > 0 ? (
                 exploration.images.map((image) => (
                   <Image
                     key={image._id}
                     src={`/uploads/${image.name}`}
-                    alt='cities'
+                    alt="cities"
                     width={400}
                     height={330}
                     className="exploration-image"
                   />
                 ))
               ) : (
-                // <Image
-                //   src={emptyImage.src}
-                //   alt="No Image Available"
-                //   width={400}
-                //   height={330}
-                //   className="exploration-image"
-                // />
                 null
               )}
             </Link>
             <div className="exploration-details">
-              <div className='explore_l'>
+              <div className="explore_l">
                 <h3>Explorations {exploration.title}</h3>
-                <p>Cities in {exploration.title} {exploration.totalCities}</p>
+                <p>Cities in {exploration.title}: {exploration.totalCities}</p>
               </div>
-              <div className='icon_custom'>
-                <img src='/images/arrowu.png' />
+              <div className="icon_custom">
+                <img src="/images/arrowu.png" alt="arrow" />
               </div>
             </div>
           </div>
-        )))}
+        ))}
       </div>
     </div>
   );
